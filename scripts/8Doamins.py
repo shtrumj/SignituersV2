773979ee3a42979ea541e7df3ffb6733264b7c4b
shtrumj@gmail.com <piano74672@>
2022-07-22 11:53:13 +0300

    with filtering

--- scripts/8Doamins.py
+++ scripts/8Doamins.py
@@ -5,6 +5,9 @@ DomainOutputFile = '../DataFiles/domains.txt'
 sha1outputFile = '../DataFiles/sha1.txt'
 sha256Outputfile = '../DataFiles/sha256.txt'
 md5Outputfile = '../DataFiles/md5.txt'
+URLOutputfile = '../DataFiles/urls.txt'
+IPSOutputfile = '../DataFiles/ips.txt'
+
 
 with open("../DataFiles/RawData/myJson.json") as f:
     data = json.load(f)
@@ -20,6 +23,11 @@ stop = norm.index.stop
 sha1= []
 sha256 = []
 md5 = []
+urls = []
+ips = []
+ipsNB = []
+urlNB = []
+urlsCl = []
 
 for first in range(0,stop):
     EMList.append(norm.indicators[first])
@@ -30,17 +38,26 @@ for list in EMList:
         if num["type"] == "domain":
             finalDomains.append(num['value'])
 
-
+for list in EMList:
+    for num in list:
+        if num["type"] == "IP":
+            ips.append(num['value'])
 
 for list in EMList:
     for num in list:
         if num["type"] == "sha1":
             sha1.append(num['value'])
 
+
 for list in EMList:
     for num in list:
         if num["type"] == "sha256":
             sha256.append(num['value'])
+
+for list in EMList:
+    for num in list:
+        if num["type"] == "url":
+            urls.append(num['value'])
 
 for list in EMList:
     for num in list:
@@ -50,12 +67,23 @@ for list in EMList:
 
 for line in finalDomains:
     finalNoBtackets.append(re.sub(r"[\[\]]",'',line))
-    print(finalNoBtackets)
+
+for line in urls:
+    urlNB.append(re.sub(r"[\[\]]",'',line))
+
+
+for line in urlNB:
+    line= line.replace("hxxps","https")
+    line=line.replace("hxxp","http")
+    line = line.replace("hXXp", "http")
+    urlsCl.append(line)
+
+for line in ips:
+    ipsNB.append(re.sub(r"[\[\]]",'',line))
 
 with open(DomainOutputFile, 'w') as output:
     for z in finalNoBtackets:
         output.writelines("%s\n" % z)
-
 
 with open(sha1outputFile, 'w') as sha1out:
     for sha in sha1:
@@ -68,3 +96,11 @@ with open(sha256Outputfile, 'w') as sha256out:
 with open(md5Outputfile, 'w') as md5out:
     for line in md5:
         md5out.writelines("%s\n" % line)
+
+with open(URLOutputfile, 'w') as urls:
+    for line in urlsCl:
+        urls.writelines("%s\n" % line)
+
+with open(IPSOutputfile, 'w') as ipsfile:
+    for line in ipsNB:
+        ipsfile.writelines("%s\n" % line)

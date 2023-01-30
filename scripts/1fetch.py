111f0b941ffdbb4a685850c04427ea8499bc2e2d
shtrumj <shtrumj@gmail.com>
2023-01-30 14:40:39 +0200

    Generalize api keys .env

--- scripts/1fetch.py
+++ scripts/1fetch.py
@@ -1,24 +1,26 @@
 import requests
 import json
 import os
+from dotenv import load_dotenv,find_dotenv
 
-file_path = '../DataFiles/RawData/myJson.json'
+env_file= find_dotenv(".env.api_fetch")
+load_dotenv(env_file)
+file_path = 'DataFiles/RawData/myJson.json'
 url = "https://cybernet.cyber.gov.il/api/rest/getIndicators?fromDate=2010-10-10_00%3A00%3A00"
 
 payload={}
 headers = {
-  'CN-USER-NAME': 'soc_trot',
-  'X-API-KEY': '0CED85C07DA0A153',
+  'CN-USER-NAME': os.getenv("API_USER"),
+  'X-API-KEY': os.getenv("API_KEY"),
   'Authorization': 'Basic Og==',
   'Cookie': 'LastMRH_Session=b8c05467; MRHSession=1da3978fd1960afc055af4c9b8c05467',
   'Content-type': 'application/json; charset=utf-8'
 }
 
 response = requests.request("GET", url, headers=headers, data=payload)
-response2 =response.json()
+response_formated =response.json()
 
-
-myjson = json.dumps(response2, ensure_ascii=False)
+myjson = json.dumps(response_formated, ensure_ascii=False)
 # file = "../DataFiles/RawData/myJson.jsonson"
 mode = 'a' if os.path.exists(file_path) else 'w'
 with open(file_path, mode, encoding='utf-8') as outfile:
